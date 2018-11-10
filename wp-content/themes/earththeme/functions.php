ae76e777a6ae3b107560c503329e8207d1be2f21
Ryzlane <ryzlane@free.fr>
2018-11-11 00:05:48 +0100

    Configuration of earth theme with timber done

--- wp-content/themes/earththeme/functions.php
+++ wp-content/themes/earththeme/functions.php
@@ -1,5 +1,4 @@
 <?php
-require_once( __DIR__ . '/vendor/autoload.php' );
 
 define( 'THEME_PATH' ,          get_template_directory()            );
 define( 'TEMPLATE_PATH' ,       THEME_PATH .    '/templates'        );
@@ -11,9 +10,30 @@ define( 'JS_URL' ,              THEME_URL .    '/assets/scripts'      );
 define( 'FAVICONS_URL' ,        THEME_URL .    '/assets/favicon'      );
 define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 
-$timber = new Timber\Timber();
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+	});
+
+	add_filter('template_include', function( $template ) {
+		return get_stylesheet_directory() . '/static/no-timber.html';
+	});
+
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array( 'templates', 'views' );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
 
 foreach ( glob( THEME_PATH . "/inc/*.php" ) as $file ) {
-    include_once $file;
+	include_once $file;
 }
-?>
--- wp-content/themes/earththeme/header.php
+++ wp-content/themes/earththeme/header.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
+<?php
+/**
+ * Third party plugins that hijack the theme will call wp_head() to get the header template.
+ * We use this to start our output buffer and render into the view/page-plugin.twig template in footer.php
+ *
+ * If you're not using a plugin that requries this behavior (ones that do include Events Calendar Pro and
+ * WooCommerce) you can delete this file and footer.php
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+$GLOBALS['timberContext'] = Timber::get_context();
+ob_start();
