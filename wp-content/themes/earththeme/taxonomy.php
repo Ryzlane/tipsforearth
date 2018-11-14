c9c0c4b623a44b7671107aa5392997e577e13190
Ryzlane Arsac-Gothière <ryzlane.arsac@hetic.net>
2018-11-14 15:23:18 +0100

    Add files via upload

--- wp-content/themes/earththeme/taxonomy.php
+++ wp-content/themes/earththeme/taxonomy.php
@@ -1,12 +1,17 @@
 <?php
 
 $args = array(
-	'taxonomy' => 'location',
-	'hide_empty' => false,
+	'tax_query' => array(
+		'taxonomy_name' => 'location',
+	)
 );
 
 $context = Timber::get_context();
 $context['taxonomy'] = Timber::get_terms( $args );
 $templates = array( 'taxonomy.twig' );
+
+echo '<pre>';
+print_r($context['taxonomy']);
+echo '</pre>';
 
 Timber::render( $templates, $context );
