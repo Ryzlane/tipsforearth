65eeb18310a9fd7e390049246843728d37532aa4
Ryzlane <ryzlane@free.fr>
2018-11-14 16:12:52 +0100

    Update home

--- wp-content/themes/earththeme/index.php
+++ wp-content/themes/earththeme/index.php
@@ -1,33 +1,3 @@
 <?php
 
-$args_last_tips = array(
-	'post_type' => 'tips',
-	'orderby' => 'post_date',
-	'order' => 'DESC',
-);
-
-$args_last_numbers = array(
-	'post_type' => 'number',
-	'numberposts' => 3,
-	'orderby' => 'post_date',
-	'order' => 'DESC',
-);
-
-$args_last_articles = array(
-	'post_type' => 'post',
-	'numberposts' => 3,
-	'orderby' => 'post_date',
-	'order' => 'DESC',
-);
-
-$context = Timber::get_context();
-$context['last_tips'] = Timber::query_post( $args_last_tips );
-$context['last_numbers'] = Timber::get_posts( $args_last_numbers );
-$context['last_articles'] = Timber::get_posts( $args_last_articles );
-
-
-$templates = array( 'index.twig' );
-if ( is_home() ) {
-	array_unshift( $templates, 'front-page.twig', 'home.twig' );
-}
-Timber::render( $templates, $context );
+echo 'index page';
