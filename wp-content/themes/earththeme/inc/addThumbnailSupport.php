3aa5a080ef870b97cb7fdb2bd19b4f6346a68e68
lisapenard <lisa.penard@hetic.net>
2018-11-24 17:58:10 +0100

    Comment section css

--- wp-content/themes/earththeme/inc/addThumbnailSupport.php
+++ wp-content/themes/earththeme/inc/addThumbnailSupport.php
@@ -3,5 +3,6 @@ add_action( 'after_setup_theme', 'thumbnails_theme_support' );
 function thumbnails_theme_support(){
     add_theme_support( 'post-thumbnails' );
 
-    add_image_size( 'tips-thumb', 200, 300, true );
+    add_image_size( 'tips-thumb', 100, 200, true );
+    add_image_size( 'post-thumb', 529, 270, true );
 }
