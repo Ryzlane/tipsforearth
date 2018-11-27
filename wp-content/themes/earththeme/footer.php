5fe67cb80ee34fe56cae90de38b8013be7d9ec79
lisapenard <lisa.penard@hetic.net>
2018-11-27 17:14:16 +0100

    Footer & Menu

--- wp-content/themes/earththeme/footer.php
+++ wp-content/themes/earththeme/footer.php
@@ -11,6 +11,15 @@
  * @since   Timber 0.1
  */
 
+$args=array(
+    'theme_location' => 'footer', // nom du slug
+    'menu' => 'footer_fr', // nom à donner cette occurence du menu
+    'menu_class' => 'menu_footer', // class à attribuer au menu
+    'menu_id' => 'menu_id' // id à attribuer au menu
+    // voir les autres arguments possibles sur le codex
+);
+wp_nav_menu($args);
+
 $timberContext = $GLOBALS['timberContext']; // @codingStandardsIgnoreFile
 if ( ! isset( $timberContext ) ) {
 	throw new \Exception( 'Timber context not set in footer.' );
@@ -19,3 +28,4 @@ $timberContext['content'] = ob_get_contents();
 ob_end_clean();
 $templates = array( 'page-plugin.twig' );
 Timber::render( $templates, $timberContext );
+
--- wp-content/themes/earththeme/single.php
+++ wp-content/themes/earththeme/single.php
@@ -9,8 +9,18 @@
  * @since    Timber 0.1
  */
 
+$args_last_articles = array(
+	'post_type' => 'post',
+	'numberposts' => 4,
+	'orderby' => 'post_date',
+	'order' => 'DESC',
+);
+
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
+$context['last_articles'] = Timber::get_posts( $args_last_articles );
+
 Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+
